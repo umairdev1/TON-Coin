4cbca505c41d4495bc39f800e8f7d5fca13603fd
umairdev1 <umairdev4@gmail.com>
2023-09-17 22:09:57 +0500

    askadd

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const { Telegraf } = require("telegraf");
 const logger = require("./Helper/logger");
+const { getChat } = require("./Helper/functions");
 const express = require("express");
 const app = express();
 
@@ -17,37 +18,126 @@ bot.start(async (ctx) => {
   }
 
   ctx.reply(
-    "Welcome To AI Bot 🧿 \n\nCommands 👾 \n/ask  ask anything from me \n/image to create image from text  \n/en to correct your grammer \n\n\nContract Devot if you want to report any BUG or change in features"
+    "Welcome To AI Bot 🧿 \n\nCommands 👾 \n/ask  ask anything from me Contract Devot if you want to report any BUG or change in features"
   );
 });
 
 bot.help((ctx) => {
   ctx.reply(
-    "\nCommands 👾 \n\n/ask  ask anything from me \n/image to create image from text  \n/en to correct your grammer \n\n\nContract Devot if you want to report any BUG or change in features "
+    "\nCommands 👾 \n\n/ask  ask anything from me \n\n\nContract Devot if you want to report any BUG or change in features "
   );
 });
+//Bot on ask command
+bot.command("ask", async (ctx) => {
+  console.log("ok");
+  const text = ctx.message.text?.replace("/ask", "")?.trim().toLowerCase();
 
+  logger.info(`Chat: ${ctx.from.username || ctx.from.first_name}: ${text}`);
+
+  if (text) {
+    ctx.sendChatAction("typing");
+    const res = await getChat(text);
+    if (res) {
+      ctx.telegram.sendMessage(
+        ctx.message.chat.id,
+        `${res}\n\n\nJoin us on Telegram\nPePe CEO`,
+        {
+          reply_to_message_id: ctx.message.message_id,
+        }
+      );
+    }
+  } else {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "Please ask anything after /ask",
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
+    );
+  }
+});
 //bot scrape
 bot.on("message", async (ctx) => {
   const messageText = ctx.message.text.toLowerCase();
 
-  const supplyPattern = /supply:\s*(\d+)/i;
+  // List of keywords to filter
+  const keywords = [
+    "sarpia",
+    "betting game",
+    "betting dapp",
+    "wallet tracker",
+    "influencers",
+    "callers",
+    "eric cryptoman",
+    "jakegagain",
+    "scam",
+    "honeypot",
+    "rug",
+    "hidden mint",
+    "delayed honeypot",
+    "hp",
+    "insidor",
+    "100x",
+    "1000x",
+    "10",
+    "20×",
+    "malicious",
+    "code",
+    "webdev",
+    "utility dev",
+    "launching soon",
+    "when",
+    "launch",
+    "wen",
+    "safe",
+    "safu",
+    "buy",
+    "shill",
+    "admins",
+    "skull",
+    "skull admins",
+    "aeons admins",
+    "ermnmusk",
+    "elon musk",
+    "vitalik",
+    "the dev",
+    "vitalik.eth",
+    "realdogen",
+    "insider",
+    "kingdom",
+    "pow",
+    "carnage",
+    "venom",
+    "utility",
+    "matt furie",
+    "beeple",
+    "whales",
+    "giga",
+    "twitter",
+    "community",
+    "dip",
+    "entry",
+    "shillers",
+    "mods",
+    "mod",
+    "ath",
+    "eca",
+    "alpha",
+    "aeons",
+    "private group",
+  ];
 
-  const supplyMatch = messageText.match(supplyPattern);
+  const userId = ctx.from.id;
 
-  const containsKeywords = /checksum|tax|eca/i.test(messageText);
+  // Check if the message contains any of the keywords
+  const containsKeywords = keywords.some((keyword) =>
+    messageText.includes(keyword)
+  );
 
-  if (supplyMatch || containsKeywords) {
-    const userId = "5789287913";
-
-    // bot.telegram.sendMessage(userId, ctx.message.text);
-    if (supplyMatch) {
-      const supplyValue = parseInt(supplyMatch[1], 10);
-      if (supplyValue >= 1000000) {
-        // Check if supply is greater than 1 million
-        bot.telegram.sendMessage(userId, ctx.message.text);
-      }
-    }
+  if (containsKeywords) {
+    bot.telegram.sendMessage(userId, ctx.message.text);
+  } else {
+    // Do nothing for messages that don't match the conditions
   }
 });
 
